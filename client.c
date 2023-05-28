8d3386cb1fb9e1fd6881f647a519f7388cf9132f
David Gomes <david.gomes@tuta.io>
2023-05-27 21:51:22 -0300

    Update client

--- client.c
+++ client.c
@@ -9,45 +9,194 @@
 #include <string.h>
 #include <arpa/inet.h>
 
-#include "ConexaoRawSocket.h"
+#include "backup.h"
 #include "utils.h"
 
 void client_run() {
-    int socket = ConexaoRawSocket("lo");
-
+    #ifdef DEBUG
     printf("[ETHBKP] Running as client...\n");
-    unsigned char rec_buffer[BUFFER_MAX_LEN];
-    char file_name[DATA_MAX_LEN];
+    #endif
 
-    message_t* message = create_message();
-    int sequence = 0;
+    backup_t *backup = create_backup();
+
+    char *command = malloc(sizeof(char) * STR_LEN);
+    test_alloc(command, "command");
+
+    char *arg = malloc(sizeof(char) * (DATA_MAX_LEN + 1));
+    test_alloc(arg, "arg");
+
+    int arg_size;
+
+    commands_enum cmd_type;
 
     for (;;) {
-        printf("[ETHBKP] Type file name: ");
-        scanf("%s", file_name);
+        printf(">>> ");
+        fgets(command, STR_LEN, stdin);
 
-        FILE* file = fopen(file_name, "r");
-        if (!file) {
-            printf("Error: %s\n", strerror(errno));
+        cmd_type = parse_command(command, arg, &arg_size);
+
+        if (arg_size > DATA_MAX_LEN) {
+            printf("Tamanho do argumento inválido\n");
             continue;
         }
 
-        make_backup_message(message, file_name, sequence);        
+        if (cmd_type == C_EXIT) {
+            printf("\nSaindo, até logo!\n");
+            break;
+        }
 
-        print_message(message);
-
-        ssize_t size = send_message(socket, message);
-        if (size < 0)
-            printf("Error: %s\n", strerror(errno));
-
-        printf("[ETHBKP] Message sent, size=%ld\n", size);
-
-        recv(socket, rec_buffer, htons(BUFFER_MAX_LEN), 0);
-
-
-
+        switch (cmd_type) {
+            case C_BACKUP:
+                client_backup(backup, arg);
+                break;
+            case C_RETRIEVE:
+                client_retrieve(backup, arg);
+                break;
+            case C_DEFINE_BACKUP_DIRECTORY:
+                client_define_backup_dir(backup, arg);
+                break;
+            case C_CHANGE_DIRECTORY:
+                client_change_directory(arg);
+                break;
+            case C_CHECK:
+                client_check(backup, arg);
+                break;
+            case C_HELP:
+                client_help();
+                break;
+            case C_INVALID:
+                printf("Comando inválido\n");
+                break;
+            default:
+                continue;
+        }
     }
 
-    destroy_message(message);
+    free(arg);
+    free(command);
+
+    free_backup(backup);
+
     return;
 }
+
+commands_enum parse_command(char* command, char* arg, int* arg_size) {
+    #ifdef DEBUG
+    printf("[ETHBKP] Parsing command\n");
+    #endif
+
+    char *cmd = strtok(command, " ");
+    char *aux = strtok(NULL, " ");
+
+    if (aux) {
+        *arg_size = strlen(aux);
+        memcpy(arg, aux, min(*arg_size, DATA_MAX_LEN));
+
+        arg[*arg_size - 1] = '\0';
+    }
+
+    commands_enum cmd_type = C_INVALID;
+
+    if (
+        !strcmp(cmd, "backup") ||
+        !strcmp(cmd, "bkp") ||
+        !strcmp(cmd, "b")
+    )
+        cmd_type = C_BACKUP;
+    else if (
+        !strcmp(cmd, "retrieve") ||
+        !strcmp(cmd, "rtv") ||
+        !strcmp(cmd, "r")
+    )
+        cmd_type = C_RETRIEVE;
+    else if (
+        !strcmp(cmd, "check") ||
+        !strcmp(cmd, "chk") ||
+        !strcmp(cmd, "c")
+    )
+        cmd_type = C_CHECK;
+    else if (!strcmp(cmd, "dfd"))
+        cmd_type = C_DEFINE_BACKUP_DIRECTORY;
+    else if (!strcmp(cmd, "cd"))
+        cmd_type = C_CHANGE_DIRECTORY;
+    else if (!strcmp(cmd, "exit"))
+        cmd_type = C_EXIT;
+    else if (
+        !strcmp(cmd, "help") ||
+        !strcmp(cmd, "h")
+    )
+        cmd_type = C_HELP;
+
+    return cmd_type;
+}
+
+void client_backup(backup_t* backup, char *path) {
+    #ifdef DEBUG
+    printf("[ETHBKP] Command: backup\n");
+    #endif
+
+    FILE* file = fopen(path, "r");
+
+    if (!file) {
+        printf("Erro ao abrir arquivo: %s\n", strerror(errno));
+        return;
+    }
+
+    make_backup_message(backup, path);        
+
+    print_message(backup->message);
+
+    ssize_t size = send_message(backup);
+
+    if (size < 0)
+        printf("Error: %s\n", strerror(errno));
+
+    #ifdef DEBUG
+    printf("[ETHBKP] Message sent, size=%ld\n", size);
+    #endif
+
+    // recv(backup->socket, backup->buffer, htons(BUFFER_MAX_LEN), 0);
+
+    return;
+}
+
+void client_retrieve(backup_t *backup, char *file_name) {
+    #ifdef DEBUG
+    printf("[ETHBKP] Command: retrieve\n");
+    #endif
+
+    return;
+}
+
+void client_check(backup_t *backup, char *file_name) {
+    #ifdef DEBUG
+    printf("[ETHBKP] Command: check\n");
+    #endif
+
+    return;
+}
+
+void client_define_backup_dir(backup_t *backup, char *dir) {
+    #ifdef DEBUG
+    printf("[ETHBKP] Command: define backup directory\n");
+    #endif
+
+    return;
+}
+
+void client_change_directory(char *dir) {
+    #ifdef DEBUG
+    printf("[ETHBKP] Command: change directory\n");
+    #endif
+
+    return;
+}
+
+void client_help() {
+    #ifdef DEBUG
+    printf("[ETHBKP] Command: help\n");
+    #endif
+
+    return;
+}
+
--- client.h
+++ client.h
@@ -3,8 +3,33 @@
 
 #include "backup.h"
 
+typedef enum commands_enum {
+    C_BACKUP,
+    C_RETRIEVE,
+    C_CHECK,
+    C_DEFINE_BACKUP_DIRECTORY,
+    C_CHANGE_DIRECTORY,
+    C_HELP,
+    C_EXIT,
+    C_INVALID
+} commands_enum;
+
 
 void client_run();
+
+commands_enum parse_command(char *command, char *arg, int* arg_size);
+
+void client_backup(backup_t *backup, char *path);
+
+void client_retrieve(backup_t *backup, char *file_name);
+
+void client_check(backup_t *backup, char *file_name);
+
+void client_define_backup_dir(backup_t *backup, char *dir);
+
+void client_change_directory(char *dir);
+
+void client_help();
 
 #endif
 
